fe982056b2fee4c4be184e7752d2c0d9adc3bc83
jiesparago05 <jiesparago@bpi.com.ph>
2025-12-11 14:05:33 +0800

    Add files via upload

---
+++ app.java
@@ -0,0 +1,11 @@
+package module2Package2;
+
+public class app {
+	 public static void main(String[] args) throws Exception {
+		    whiteboard wb = new whiteboard();    
+		    int total = wb.addNumbers(5, 3);
+
+		    System.out.println("The total is: " + String.valueOf(total));
+
+		    }
+}
